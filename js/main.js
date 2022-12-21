17c7e21eb5a3c1033069998a2b650fc110cadc62
thunder <wjddnei@naver.com>
2022-12-21 18:04:58 +0900

    초기 작업

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper('.promotion .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 3000
+  },
+  slidesPerView: 1,
+  spaceBetween: 0,
+  centeredSlides: true,
+});
