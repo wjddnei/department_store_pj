01a7174a85e0a1fdef2a509a34108e5a3501aed8
thunder <wjddnei@naver.com>
2022-12-26 18:22:38 +0900

    js 수정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 new Swiper('.promotion .swiper', {
-  direction: 'horizontal',
-  loop: true,
-  autoplay: {
-    delay: 3000
-  },
-  slidesPerView: 1,
-  spaceBetween: 0,
-  centeredSlides: true,
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true
 });
 
 const headerEl = document.querySelector('header');
@@ -14,56 +14,39 @@ const mainMenuEl = document.querySelector('.main-menu');
 const subMenuEls = document.querySelectorAll('.item__contents');
 
 mainMenuEl.addEventListener('mouseover', function () {
-  headerEl.style.height = '380px';
+    headerEl.style.height = '380px';
 
-  subMenuEls.forEach(function (subMenuEl) {
-    subMenuEl.style.opacity = '1';
-    subMenuEl.style.visibility = 'visible';
-  });
+    subMenuEls.forEach(function (subMenuEl) {
+        subMenuEl.style.opacity = '1';
+        subMenuEl.style.visibility = 'visible';
+    });
 });
 
 headerEl.addEventListener('mouseleave', function () {
-  headerEl.style.height = '150px';
+    headerEl.style.height = '150px';
 
-  subMenuEls.forEach(function (subMenuEl) {
-    subMenuEl.style.opacity = '0';
-    subMenuEl.style.visibility = 'hidden';
+    subMenuEls.forEach(function (subMenuEl) {
+        subMenuEl.style.opacity = '0';
+        subMenuEl.style.visibility = 'hidden';
+    });
+});
+
+const iconsEls = document.querySelectorAll('.icons-group');
+const guideEls = document.querySelectorAll('.guide-group');
+
+iconsEls.forEach(function(iconsEl){
+  iconsEl.addEventListener('click',function(){
+    guideEls.forEach(function(guideEl){
+      guideEl.classList.remove('active');
+    });
+    iconsEl.nextElementSibling.classList.add('active');
   });
 });
 
-const oneIcon = document.querySelector('.one-icons');
-const oneGuideEl = document.querySelector('.one_guide');
-const twoIcon = document.querySelector('.two-icons');
-const twoGuideEl = document.querySelector('.two_guide');
-const thrIcon = document.querySelector('.thr-icons');
-const thrGuideEl = document.querySelector('.thr_guide');
-const fouIcon = document.querySelector('.fou-icons');
-const fouGuideEl = document.querySelector('.fou_guide');
-const fivIcon = document.querySelector('.five-icons');
-const fivGuideEl = document.querySelector('.fiv_guide');
-
-oneIcon.addEventListener('click',function(){
-oneGuideEl.classList.toggle('active');
-});
-twoIcon.addEventListener('click',function(){
-  twoGuideEl.classList.toggle('active');
-});
-thrIcon.addEventListener('click',function(){
-  thrGuideEl.classList.toggle('active');
-});
-fouIcon.addEventListener('click',function(){
-  fouGuideEl.classList.toggle('active');
-});
-fivIcon.addEventListener('click',function(){
-  fivGuideEl.classList.toggle('active');
-});
-
 const spyEls = document.querySelectorAll('section.scroll-spy');
-spyEls.forEach(function(spyEl){
-  new ScrollMagic.Scene({
-    triggerElement: spyEl,
-    triggerHook: 0.8
-  })
-  .setClassToggle(spyEl, 'show')
-  .addTo(new ScrollMagic.Controller());
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({triggerElement: spyEl, triggerHook: 0.8})
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
 });
